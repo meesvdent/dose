bdef0766b87dde2fd3541650b91cdc91e7fdde13
meesvdent <meesvdent@gmail.com>
2020-03-08 23:11:13 +0100

    thalf

--- visualization.py
+++ visualization.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plot_conc(t, X, stepsize):
+def plot_conc(t, X, stepsize=1):
     plt.plot(t, X)
-    plt.xticks(np.arange(0, max(t), stepsize*3600), [str(n).zfill(2) + ':00' for n in np.arange(0, max(t)/3600, stepsize)], rotation = 45)
+    plt.xticks(np.arange(0, max(t), stepsize*3600), [str(n).zfill(2) + ':00' for n in np.arange(0, max(t)/3600, stepsize)], rotation=45)
     plt.show()
