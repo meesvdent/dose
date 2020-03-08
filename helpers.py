e1baa413d79c72ca9f6725cfa0f774ded04cd3a5
florian <florianvdent@gmail.com>
2020-03-08 16:52:53 +0100

    OneCompModel functional, and cleanup

--- helpers.py
+++ helpers.py
@@ -1,9 +1,25 @@
 
-def calc_dose_conc(dose_mass, molecularMass, DV):
-    doseMoll = []
-    for mass in dose_mass:
-        doseMoll.append(mass/molecularMass)
+def mol_to_conc(mol_amount, dis_vol):
+    '''Takes iterable of molair amounts and return iterable of
+    conc coresponding to given distribution volume.
+
+    :param mol_amount:
+    :param dis_vol:
+    :return: dose_conc
+    '''
     dose_conc = []
-    for mol in doseMoll:
-        dose_conc.append(mol/DV)
+    for mol in mol_amount:
+        dose_conc.append(mol/dis_vol)
     return dose_conc
+
+
+def mass_to_mol(mass_list, molecular_mass):
+    dose_moll = []
+    for mass in mass_list:
+        dose_moll.append(mass / molecular_mass)
+    return dose_moll
+
+
+def calc_dose_conc(dose_masses, mol_mass, dis_vol):
+    dose_mol = mass_to_mol(dose_masses, mol_mass)
+    return mol_to_conc(dose_mol, dis_vol)
--- test_models.py
+++ test_models.py
@@ -2,5 +2,24 @@ from unittest import TestCase
 
 
 class TestOneCompModel(TestCase):
-    def test_intergrate(self):
-        self.fail()
+    def test_intergrate_zero_kout(self):
+        from helpers import calc_dose_conc
+        from models import OneCompModel
+        import numpy as np
+        t = np.linspace(0, 1, 1000)
+
+        dose = [0.160, 0.160]  # grams, seconds
+        time = [0.001, 0.4]
+        molecular_mass = 194.19  # Caffeine
+
+        patient_mass = 75  # kg
+        DV = 0.625 * patient_mass  # L/kg, for caffeine
+
+        dose_conc = calc_dose_conc(dose, molecular_mass, DV)
+        time_conc = [list(a) for a in zip(time, dose_conc)]
+
+        model = OneCompModel(time_conc, 0, 0.5)
+
+        X, infodict = model.intergrate(t)
+
+        self.assertAlmostEqual(np.max(X), 1.149E-5, 4)
