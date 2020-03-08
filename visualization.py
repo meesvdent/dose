9fa07e7a9bf5289ed6188b9162689b1439982de3
meesvdent <meesvdent@gmail.com>
2020-03-08 23:13:06 +0100

    visualisation

--- visualization.py
+++ visualization.py
@@ -5,3 +5,4 @@ def plot_conc(t, X, stepsize=1):
     plt.plot(t, X)
     plt.xticks(np.arange(0, max(t), stepsize*3600), [str(n).zfill(2) + ':00' for n in np.arange(0, max(t)/3600, stepsize)], rotation=45)
     plt.show()
+
