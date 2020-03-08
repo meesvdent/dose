829a7fedcea5ad22ff595ea6e63f1171512bcfa0
meesvdent <meesvdent@gmail.com>
2020-03-08 14:43:57 +0100

    delta_abs func

--- models.py
+++ models.py
@@ -10,10 +10,14 @@ class OneCompModel(object):
     def __str__(self):
         return f"model parameters: {self.doses}, {self.kout}, {self.kabs}"
 
+    def calc_delta_abs(self, conc, t, dt):
+        delta_abs = conc * np.exp(-self.kabs * (t - dt))
+        return delta_abs
+
     def calc_unabs(self, t):
         tot = 0
         for dt, conc in self.doses:
-            tot += self.step(t - dt) * conc * np.exp(-self.kabs * (t - dt))
+            tot += self.step(t - dt) * self.calc_delta_abs(conc, t, dt)
         return tot
 
     def d_abs(self, amount_unabs):
@@ -26,7 +30,7 @@ class OneCompModel(object):
 
 t = np.linspace(0, 1, 1000)
 
-model = OneCompModel([[0.001, 5]], 0, 1)
+model = OneCompModel([[0.001, 5], [0.4, 5]], 0, 1)
 amount_unabs = model.calc_unabs(t)
 delta_abs = model.d_abs(amount_unabs)
 print(amount_unabs)
